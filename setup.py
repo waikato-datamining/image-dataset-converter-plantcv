db752511e6f9b9c8c231b0cee6a8486af09dce36
Peter Reutemann <fracpete@gmail.com>
2025-10-31 14:57:36 +1300

    prepared for release

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "image_dataset_converter",
         "plantcv",
     ],
-    version="0.0.1",
+    version="0.1.0",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
     entry_points={
